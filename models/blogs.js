0dbe1ed78d85d109df7a3debdd2bcc51859eb924
ckymn <cokyamanmuhammet@gmail.com>
2021-04-16 05:17:32 +0300

    code edit 🎉🔥

--- models/blogs.js
+++ models/blogs.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const blogSchema = new mongoose.Schema({
     title : {
         type : String,
-        require : true
+        require : true,
     },
     short : {
         type : String,
@@ -12,7 +12,25 @@ const blogSchema = new mongoose.Schema({
     long : {
         type : String,
         require : true
+    },
+    site : {
+        type : String,
+        require : true,
+        uppercase : true
+    },
+    g_link : {
+        type : String,
+        require : true
+    },
+    s_link:{
+        type: String,
+        require : true
+    },
+    t_link:{
+        type: String,
+        require : true
     }
+
 },{timestamps : true});
 
 
